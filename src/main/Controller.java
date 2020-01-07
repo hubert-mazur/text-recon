096d74d82fc336ac23c6bb7b96260c484b291a96
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2020-01-07 23:25:58 +0100

    Proper button behaviour

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -4,6 +4,8 @@ import binarize.Img;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -14,14 +16,42 @@ public class Controller {
     @FXML
     private ImageView imageView;
 
+    private Img img;
+
     public void onFileChooseRequest(ActionEvent event) {
         FileChooser fileChooser = new FileChooser();
         File imgFile = fileChooser.showOpenDialog(new Stage());
-        Img img = new Img(imgFile);
-        imageView.setImage(img.getBinarizedImage());
+        img = new Img(imgFile);
+        this.onOriginalImageRequest(event);
+    }
+
+    private void showImageNotLoadedDialog() {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Informacja");
+        alert.setHeaderText("Najpierw wczytaj obraz");
+        alert.setContentText("Plik -> Wczytaj obraz");
+        alert.showAndWait();
     }
 
     public void setOnCloseRequest(ActionEvent event) {
         Platform.exit();
+    }
+
+    public void onOriginalImageRequest(ActionEvent event) {
+        if (img == null) {
+            showImageNotLoadedDialog();
+            return;
+        }
+
+        imageView.setImage(img.getInputImage());
+    }
+
+    public void onGrayscaleImageRequest(ActionEvent event) {
+        if (img == null) {
+            showImageNotLoadedDialog();
+            return;
+        }
+
+        imageView.setImage(img.getBinarizedImage());
     }
 }
