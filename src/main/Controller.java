c6dbcaadfa23cbf292570a1d8144bcc9e2949da4
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2020-01-07 22:45:32 +0100

    Load and display image from system in GUI

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -1,4 +1,27 @@
 package main;
 
+import binarize.Img;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
 public class Controller {
+    @FXML
+    private ImageView imageView;
+
+    public void onFileChooseRequest(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        File imgFile = fileChooser.showOpenDialog(new Stage());
+        Img img = new Img(imgFile);
+        imageView.setImage(img.getBinarizedImage());
+    }
+
+    public void setOnCloseRequest(ActionEvent event) {
+        Platform.exit();
+    }
 }
